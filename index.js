bbfb280475492fd8f201492d9bf25861c026e458
dann9422 <dannsk82294@gmail.com>
2021-12-27 20:44:44 -0600

    creando vistas y pagina de nosotros

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 // const express = require("express");// forma vieja
 import express from "express";
 import router from "./routes/index.js";
+import db from './config/db.js';
 const app = express();
 // definir el puerto
 
 const port = process.env.port || 4000;
 
+// conectar la base de datos
+
+db.authenticate()
+.then(()=> console.log('base de datos conectada'))
+.catch(error=> console.log(error));
+
 // habilitar pug
 
 app.set("view engine", "pug");
+//definir la carpeta publica
+
+
+//obtener el anio
+
+app.use((req,res,next)=>{
+const year= new Date();
+res.locals.actualyear =year.getFullYear();
+res.locals.nombresitio ='Agencia de viajes'
+next();
+})
+app.use(express.static('Public'));
 
 app.use("/", router);
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,38 @@
 import express from "express";
 
 const router = express.Router();
-router.get("/", (req, res) => {
+router.get("/inicio", (req, res) => {
   //req- loq ue enviamos res lo que responde
-  res.render("inicio");
+  res.render('inicio',{
+    pagina: 'Inicio'
+  });
 });
 
 router.get("/nosotros", (req, res) => {
   //req- loq ue enviamos res lo que responde
-  res.render("nosotros");
+  res.render("nosotros",{
+    pagina: 'Nosotros'
+  });
 });
+
+router.get("/viajes", (req, res) => {
+  //req- loq ue enviamos res lo que responde
+  res.render("viajes",{
+    pagina: 'Viajes'
+  });
+});
+
+router.get("/testimoniales", (req, res) => {
+  //req- loq ue enviamos res lo que responde
+  res.render("testimoniales",{
+    pagina: 'Testimoniales'
+  });
+});
+
+router.get("/contacto", (req, res) => {
+  //req- loq ue enviamos res lo que responde
+  res.render("contacto");
+});
+
 
 export default router;
