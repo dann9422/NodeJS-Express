97c07fc11fb7f6e034a8a6b6c985cd14b3311b8d
dann9422 <dannsk82294@gmail.com>
2021-12-27 22:58:30 -0600

    conectando a la base de datos

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,15 @@
 import express from "express";
-
+import { paginaInicio,paginaNosotros,paginaViajes,paginaTestimoniales } from "../controllers/paginasController.js";
 const router = express.Router();
-router.get("/inicio", (req, res) => {
-  //req- loq ue enviamos res lo que responde
-  res.render('inicio',{
-    pagina: 'Inicio'
-  });
-});
+router.get("/inicio",paginaInicio );
 
-router.get("/nosotros", (req, res) => {
-  //req- loq ue enviamos res lo que responde
-  res.render("nosotros",{
-    pagina: 'Nosotros'
-  });
-});
+router.get("/nosotros",paginaNosotros );
 
-router.get("/viajes", (req, res) => {
-  //req- loq ue enviamos res lo que responde
-  res.render("viajes",{
-    pagina: 'Viajes'
-  });
-});
+router.get("/viajes",paginaViajes);
 
-router.get("/testimoniales", (req, res) => {
-  //req- loq ue enviamos res lo que responde
-  res.render("testimoniales",{
-    pagina: 'Testimoniales'
-  });
-});
+router.get("/testimoniales", paginaTestimoniales);
 
-router.get("/contacto", (req, res) => {
-  //req- loq ue enviamos res lo que responde
-  res.render("contacto");
-});
+router.get("/contacto", );
 
 
 export default router;
